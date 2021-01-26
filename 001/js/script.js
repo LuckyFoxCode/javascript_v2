e40720b1804646e72d5f467631a0b97455971552
SERGO <sergei181989@gmail.com>
2021-01-26 23:32:55 +0200

    Continue to study the video course
    Продолжение изучение видео-курса и выполнение д/з по js

--- 001/js/script.js
+++ 001/js/script.js
@@ -1,21 +1,26 @@
 'use strict';
 
-const btn = document.querySelector('button'),
-      one = document.querySelector('#one'),
-      two = document.querySelector('#two'),
-      text = document.querySelector('.text'),
-      select = document.querySelector('#select');
+const btn = document.querySelector('.btn_one'),
+  inputCheck = document.querySelector('#check'),
+  inputPass = document.querySelector('#pass'),
+  inputOne = document.querySelector('#text-one'),
+  inputText = document.querySelector('#text'),
+  select = document.querySelector('#select'),
+  out = document.querySelector('.out'),
+  outSecond = document.querySelector('.out-second'),
+  form = document.querySelector('.form');
 
 btn.addEventListener('click', () => {
-  let numOne = +one.value,
-      numTwo = +two.value,
-      selected = select.value;
-
-  if (selected === '+') {
-    text.innerHTML = numOne + numTwo;
-  } else {
-    text.innerHTML = numOne - numTwo;
-  }
-
+  inputText.innerHTML = inputOne.value;
+  out.innerHTML = inputText.value;
+  console.log(inputOne.value);
 });
 
+select.addEventListener('change', () => {
+  inputOne.value = select.value;
+});
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  outSecond.innerHTML = `${inputOne.value} ${inputPass.value}`;
+});
