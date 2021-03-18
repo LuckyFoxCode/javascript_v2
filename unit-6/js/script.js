6a639c03279ae1a9043d4da4a16b73343dce22ce
13redfox <sergei181989@gmail.com>
2021-03-18 16:54:46 +0200

    feat: add unit-6 DONE! 10/10

---
+++ unit-6/js/script.js
@@ -0,0 +1,167 @@
+'use strict';
+/*
+ ! Lessons:
+ + for
+ + while
+ + do while
+*/
+
+/*
+const out = document.querySelector('.out');
+
+for (let i = 0; i < 5; i++) {
+	for (let k = 0; k < 10; k++) {
+		out.innerHTML += k;
+	}
+	out.innerHTML += ' ';
+}
+
+for (let i = 1; i <= 10; i++) {
+	for (let k = 1; k <= 10; k++) {
+		out.innerHTML += `${i} * ${k} = ${k * i} <br>`;
+	}
+	out.innerHTML += `<hr>`;
+}
+*/
+
+/*
+ ! Home task
+*/
+
+const out = document.querySelector('.out');
+
+// *	Task 1
+
+// for (let i = 0; i < 3; i++) {
+// 	for (let k = 0; k < 3; k++) {
+// 		out.innerHTML += '*';
+// 	}
+// 	out.innerHTML += '_';
+// }
+
+// *	Task 2
+
+// for (let i = 0; i < 3; i++) {
+// 	out.innerHTML += `<br>${i + 1} <br>`;
+// 	for (let k = 0; k < 3; k++) {
+// 		out.innerHTML += `*_`;
+// 	}
+// }
+
+// *	Task 3
+
+// for (let i = 0; i < 4; i++) {
+// 	for (let k = 0; k < 3; k++) {
+// 		out.innerHTML += '*_';
+// 	}
+// 	out.innerHTML += '<br>';
+// }
+
+// *	Task 4
+
+// for (let i = 1; i < 4; i++) {
+// 	out.innerHTML += `${i}_`;
+// 	for (let k = 1; k < 6; k++) {
+// 		out.innerHTML += `${k} `;
+// 	}
+// }
+
+// *	Task 5
+
+// for (let i = 0; i < 3; i++) {
+// 	for (let k = 0; k < 3; k++) {
+// 		out.innerHTML += '10';
+// 	}
+// 	out.innerHTML += `<br>`;
+// }
+
+// *	Task 6
+
+// for (let i = 0; i < 3; i++) {
+// 	for (let k = 0; k < 6; k++) {
+//
+// 		// if (k === 0) {
+// 		// 	out.innerHTML += '1';
+// 		// } else if (k === 1) {
+// 		// 	out.innerHTML += '0';
+// 		// } else if (k === 2) {
+// 		// 	out.innerHTML += 'x';
+// 		// } else if (k === 3) {
+// 		// 	out.innerHTML += '0';
+// 		// } else if (k === 4) {
+// 		// 	out.innerHTML += '1';
+// 		// } else {
+// 		// 	out.innerHTML += 'x';
+// 		// }
+//
+// 		switch (k) {
+// 			case 0:
+// 			case 4:
+// 				out.innerHTML += '1';
+// 				break;
+// 			case 1:
+// 			case 3:
+// 				out.innerHTML += '0';
+// 				break;
+// 			case 2:
+// 			case 5:
+// 				out.innerHTML += 'x';
+// 				break;
+// 		}
+// 	}
+// 	out.innerHTML += '<br>';
+// }
+
+// *	Task 7
+
+// for (let i = 0; i <= 4; i++) {
+// 	for (let k = 0; k < i; k++) {
+// 		out.innerHTML += '*';
+// 	}
+// 	out.innerHTML += '<br>';
+// }
+
+// *	Task 8
+
+// for (let i = 0; i <= 4; i++) {
+// 	for (let k = 5; k > i; k--) {
+// 		out.innerHTML += '*';
+// 	}
+// 	out.innerHTML += '<br>';
+// }
+
+// *	Task 9
+
+// for (let i = 0; i <= 6; i++) {
+// 	for (let k = 1; k < i; k++) {
+// 		out.innerHTML += `${k} `;
+// 	}
+// 	out.innerHTML += '<br>';
+// }
+
+// *	Task 10
+
+// for (let i = 1, j = 0; i <= 5; i++, j++) {
+// 	for (let k = 1; k <= 10; k++) {
+// 		if (k < 10) {
+// 			out.innerHTML += `${j}${k} `;
+// 		} else {
+// 			out.innerHTML += `${k * i} `;
+// 		}
+// 	}
+// 	out.innerHTML += '<br>';
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
