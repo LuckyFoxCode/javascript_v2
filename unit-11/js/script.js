16e75976a12fcc85743f441de520b750df8a55e0
13redfox <sergei181989@gmail.com>
2021-03-20 18:29:23 +0200

    feat: add unit-11

---
+++ unit-11/js/script.js
@@ -0,0 +1,197 @@
+'use strict';
+/*
+ ! Lessons:
+ + .pop()
+ + .push()
+ + .splice()
+*/
+
+/*
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const b = ['a', 'b', 'c', 'f', 'g', 'k'];
+
+console.log(a.length);
+a.push(10,4,98,4,655,45,5675);
+console.log(a.length);
+console.log(a);
+
+b.pop();
+console.log(b);
+
+delete a[3];
+console.log(a);
+
+a.splice(3,3,'hello');
+console.log(a);
+ */
+
+/*
+ ! Home task
+*/
+
+const btn = document.querySelector('.btn');
+const out = document.querySelector('.out');
+const inputText = document.querySelector('.in-text');
+
+const d1 = [33, 'best', 66, 'best'];
+
+function showArr(domElem, arr) {
+	let str = '';
+	for (let i = 0; i < arr.length; i++) {
+		str += arr[i] + ' ';
+	}
+	domElem.innerHTML = str;
+}
+
+// *	Task 1
+
+// btn.onclick = () => {
+// 	d1.push(inputText.value);
+//
+// 	showArr(out, d1);
+// };
+
+// *	Task 2
+
+// btn.onclick = () => {
+// 	d1.pop();
+// 	showArr(out, d1);
+// };
+
+// *	Task 3
+
+// btn.onclick = () => {
+// 	d1.shift();
+// 	showArr(out, d1);
+// };
+
+// *	Task 4
+
+// btn.onclick = () => {
+// 	d1.push(inputText.value);
+// 	inputText.value = '';
+//
+// 	showArr(out, d1);
+// };
+
+// *	Task 5
+
+// btn.onclick = () => {
+// 	d1.unshift(inputText.value);
+// 	inputText.value = '';
+//
+// 	showArr(out, d1);
+// };
+
+// *	Task 6
+
+// const d6 = ['test', 5, 12];
+//
+// btn.onclick = () =>{
+// 	let length = d6.length;
+// 	d6[length] = inputText.value;
+// 	inputText.value = '';
+//
+// 	showArr(out, d6);
+// };
+
+// *	Task 7
+
+// const d7 = ['china', 'india', 'brazil', 'japan', 'egypt'];
+//
+// btn.onclick = () => {
+// 	let length = d7.length - 1;
+// 	d7.splice(length, 1);
+// 	showArr(out, d7);
+// };
+
+// *	Task 8
+
+// let d8 = [2, '4', 12, 67, 'hello'];
+//
+// btn.onclick = () => {
+// 	let a = [];
+// 	a[0] = inputText.value;
+// 	let count = 1;
+// 	for (let i = 0; i < d8.length; i++) {
+// 		a[count] = d8[i];
+// 		count++;
+// 	}
+// 	showArr(out, a);
+// };
+
+// *	Task 9
+
+// const d9 = [100, 200, 300, 400, 700, 121];
+//
+// btn.onclick = () => {
+// 	d9.splice(0, 1);
+// 	showArr(out, d9);
+// };
+
+// *	Task 10
+
+
+// const d10 = [3, 14, 15, 92, 6];
+//
+// btn.onclick = () => {
+// 	d10.reverse();
+// 	showArr(out, d10);
+// };
+
+// *	Task 11
+
+// const d11 = [2, 3, 4, 5, 6, 7];
+//
+// btn.onclick = () => {
+// 	let num = +inputText.value;
+// 	out.innerHTML = d11.indexOf(num);
+// };
+
+// *	Task 12
+
+const d12 = [6, 62, 60, 70, 1, 5];
+
+btn.onclick = () => {
+	let str = '';
+	let num = +inputText.value;
+
+	for (let i = 0; i < d12.length; i++) {
+		if (d12[i] === num) {
+			 str = i;
+		}
+	}
+	out.innerHTML = str;
+};
+
+
+// *	Task 13
+
+const d13 = [6, 0, 22, 1, 4, 76];
+
+btn.onclick = () => {
+	const arr = [];
+	let count = d13.length - 1;
+	console.log(count);
+
+
+	for (let i = 0; i <= d13.length; i++) {
+
+		arr[count] += arr.push(d13[i]);
+		count--;
+	}
+	out.innerHTML = arr;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
